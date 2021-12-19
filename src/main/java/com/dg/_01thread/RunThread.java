b61e78238fb027fbde6ef741afc3bd49fbee7b00
YanHui-Yang <124481952@qq.com>
2021-12-19 15:10:08 +0800

    feat: dobble invoke start() will throw IllegalThreadStateException

--- src/main/java/com/dg/_01thread/RunThread.java
+++ src/main/java/com/dg/_01thread/RunThread.java
@@ -5,6 +5,8 @@ public class RunThread {
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
         myThread.start();
+        // 多次执行start方法，会出现异常Exception in thread "main" java.lang.IllegalThreadStateException
+//        myThread.start();
         System.out.println("运行结束！");
     }
 }
